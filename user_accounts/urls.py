74ee5a2f3118df096b1be013db3a12ba5d6c55b9
birrusaisree25-beep <birru.saisree25@gmail.com>
2026-01-12 20:13:24 -0500

    feature : urls.py

--- user_accounts/urls.py
+++ user_accounts/urls.py
@@ -1,4 +1,4 @@
-from user_accounts.views import signup,signin,forgot_password,reset_password
+from user_accounts.views import signup,signin,forgot_password,reset_password,verify_otp
 
 from django.urls import path
 
@@ -6,6 +6,7 @@ urlpatterns = [
     path('signup/', signup, name= 'signup'), #endpoint,function names,identifier
     path('signin/', signin, name= 'signin'),
     path('forgot-password/',forgot_password, name='forgot_password'),
-    path('reset-password/',reset_password, name='reset_password')
+    path('reset-password/',reset_password, name='reset_password'),
+    path('otp/', verify_otp, name='otp')
     
 ]
